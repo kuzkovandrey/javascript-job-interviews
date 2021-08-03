b654e69bcda715544308b41e2b90cd56b1284568
Andrey Kuzkov <72691736+kuzkovandrey@users.noreply.github.com>
2021-08-03 21:54:57 +0300

    Update findVowels.js

--- findVowels.js
+++ findVowels.js
@@ -1,6 +1,5 @@
-let findVowels = (string, reference = ["a", "e", "i", "o", "u"]) => {
-	return string.split('').filter(item => reference.includes(item)).length
+let findVowels = (string, vowels = ["a", "e", "i", "o", "u"]) => {
+	return string.split('').filter(letter => vowels.includes(letter)).length
 }
-
 console.log(findVowels('hello'))
 console.log(findVowels('why'))
